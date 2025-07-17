70ff578ee83557b723076f75bf7dd49a45479b74
Octarudin Mahendra <31201662+octarudin@users.noreply.github.com>
2025-07-17 09:55:30 +0700

    sprint1/feat-input-by-keyboard: input value manual

--- header/kernel.h
+++ header/kernel.h
@@ -7,6 +7,7 @@
 
 #include "shared.h"
 
+using namespace std;
 using namespace utils;
 
 namespace kernel
@@ -14,6 +15,12 @@ namespace kernel
     void startApp(); // start the application
     void init(); // initialization the apps
     void eventRoutine(); // event routines
+
+    void checkTheKernelState(); // check the kernel state
+
+    void fillStudentScore(); // fill the current student score 
+    void calculateStudentGrade(); // convert the current student score to alphabethical grade
+    void printStudentGrade(); // print the all student grade
 
 } // namespace kernel
 
--- header/utils.h
+++ header/utils.h
@@ -16,9 +16,18 @@ namespace utils
     void setMutexState(MutexState newState); // set mutex state
     MutexState getMutexState(); // get mutex state
     void printString(string param); // print a string
+    void setCounter(unsigned int number); // set counter value
     unsigned int getCounter(); // get counter value
-    void setCounter(unsigned int number); // set a counter number
-    void increaseCounter(); // increase counter value by 1 point
+    void increaseCounter(); // increase the counter by 1 point
+
+    void setStudentQuantity(unsigned char number); // set a counter number
+    unsigned char getStudentQuantity(); // set a counter number
+    void setStudentScore(unsigned char number); // set student-X score by code, X = 1, 2, ..., 255
+    void setStudentScore(); // set current student score
+    unsigned char getStudentScore(unsigned char index); // get student-X score, X = 1, 2, ..., 255
+    unsigned char getStudentScore(); // get current student score
+    void setStudentGrade(char number); // set student-X grade, X = 1, 2, ..., 255
+    char getStudentGrade(unsigned char index); // get student-X grade, X = 1, 2, ..., 255 
 
 } // namespace utils
 
